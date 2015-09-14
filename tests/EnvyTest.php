6ef69a34b48b517b63c284bab88e4d43b319e12d
Marijn Ophorst <marijn@sensimedia.nl>
2015-09-14 09:52:00 +0200

    add unit tests

---
+++ tests/EnvyTest.php
@@ -0,0 +1,48 @@
+<?php
+
+use Envy\Envy;
+
+class EnvyTest extends PHPUnit_Framework_TestCase
+{
+    private function config($env)
+    {
+        return function () use ($env) {
+            return $env;
+        };
+    }
+
+    private function runtests($config)
+    {
+        $config = dirname(__FILE__)."/$config";
+        foreach (['test' => 'bar', 'prod' => 'baz'] as $env => $check) {
+            $envy = new Envy($config, $this->config($env));
+            $this->assertEquals($check, $envy->foo);
+        }
+    }
+
+    public function testJson()
+    {
+        $this->runtests('json.json');
+    }
+
+    public function testIni()
+    {
+        $this->runtests('ini.ini');
+    }
+
+    public function testPhp()
+    {
+        $this->runtests('php.php');
+    }
+
+    public function testYaml()
+    {
+        $this->runtests('yaml.yml');
+    }
+
+    public function testXml()
+    {
+        $this->runtests('xml.xml');
+    }
+}
+
